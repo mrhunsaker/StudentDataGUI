9b1bbaff496cf341a4cc9e60ef8838614506c5b0
mrhunsaker <hunsakerconsulting@gmail.com>
2023-02-23 17:39:48 -0700

    Variables stores in helpers.py as much as possible and called via globals() rather than locals()

--- main.py
+++ main.py
@@ -10746,46 +10746,6 @@ class observationsPanel(scrolled.ScrolledPanel):
             self.dial.ShowModal()
 
 
-class DataTable(wx.grid.GridTableBase):
-    def __init__(self, data = None):
-        wx.grid.GridTableBase.__init__(self)
-        self.headerRows = 1
-        if data is None:
-            data = pd.DataFrame()
-        self.data = data
-
-    def GetNumberRows(self):
-        return len(self.data)
-
-    def GetNumberCols(self):
-        return len(self.data.columns) + 1
-
-    def GetValue(self, row, col):
-        if col == 0:
-            return self.data.index[row]
-        return self.data.iloc[row, col - 1]
-
-    def SetValue(self, row, col, value):
-        self.data.iloc[row, col - 1] = value
-
-    def GetColLabelValue(self, col):
-        if col == 0:
-            if self.data.index.name is None:
-                return 'Index'
-            else:
-                return self.data.index.name
-        return str(self.data.columns[col - 1])
-
-    def GetTypeName(self, row, col):
-        return wx.grid.GRID_VALUE_STRING
-
-    def GetAttr(self, row, col, prop):
-        attr = wx.grid.GridCellAttr()
-        if row % 2 == 1:
-            attr.SetBackgroundColour(EVEN_ROW_COLOUR)
-        return attr
-
-
 class StudentDataBook(
         wx.Frame,
         wx.Accessible
